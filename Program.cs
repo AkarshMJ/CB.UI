101b41b672dccd88297939a45ded3860e74d6009
AkarshMJ <129953993+AkarshMJ@users.noreply.github.com>
2026-01-07 11:55:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Student_Marks_Management_System.Data.Model;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// SQL Server connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Students}/{action=Index}/{id?}");
+
+app.Run();
